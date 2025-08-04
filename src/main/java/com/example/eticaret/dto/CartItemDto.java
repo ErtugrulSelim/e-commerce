3f7b97e304c0f124745e758e141ae38f1db9e329
ErtugrulSelim <e.selimbark@gmail.com>
2025-08-04 14:36:30 +0300

    Updated.

--- src/main/java/com/example/eticaret/dto/CartItemDto.java
+++ src/main/java/com/example/eticaret/dto/CartItemDto.java
@@ -1,7 +1,6 @@
 package com.example.eticaret.dto;
 
 import com.example.eticaret.model.Cart;
-import com.example.eticaret.dto.ProductDto;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 
