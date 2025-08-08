32d08448844668c304753ca69ee01bc737dcab39
ErtugrulSelim <e.selimbark@gmail.com>
2025-08-08 16:39:24 +0300

    Feed Back system added.

--- src/main/java/com/example/eticaret/service/EmailSenderService.java
+++ src/main/java/com/example/eticaret/service/EmailSenderService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailSenderService {
 
-    @Autowired
     private JavaMailSender mailSender;
 
     public void sendEmail(MailDto mailDto) {
