77f45436af7bfa1fcf02399e1b3c0fa1f65cecf8
ErtugrulSelim <e.selimbark@gmail.com>
2025-08-08 16:37:19 +0300

    Feed Back system added.

--- src/main/java/com/example/eticaret/service/EmailSenderService.java
+++ src/main/java/com/example/eticaret/service/EmailSenderService.java
@@ -2,7 +2,6 @@ package com.example.eticaret.service;
 
 import com.example.eticaret.dto.MailDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
